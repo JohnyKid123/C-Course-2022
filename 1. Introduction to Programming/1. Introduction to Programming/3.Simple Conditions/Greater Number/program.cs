8204819f242c568b8fd0d50ad1618ebf29f6c96a
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 18:31:27 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/3.Simple Conditions/Greater Number/program.cs
@@ -0,0 +1,20 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var num1= double.Parse(Console.ReadLine());
+		var num2= double.Parse(Console.ReadLine());
+		if(num1>num2)
+		{
+			Console.WriteLine(num1);
+		}
+		else
+		{
+			Console.WriteLine(num2);
+		}
+		
+	}
+						  
+}
