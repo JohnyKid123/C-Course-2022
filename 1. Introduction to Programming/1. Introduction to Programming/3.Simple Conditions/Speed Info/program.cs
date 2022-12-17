a138e4b79a6224eb6ba42f8553dd4a813d616289
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 18:45:47 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/3.Simple Conditions/Speed Info/program.cs
@@ -0,0 +1,31 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var s = double.Parse(Console.ReadLine());
+		if(s<=10)
+		{
+			Console.WriteLine("slow");
+		}
+		else if(s>10 & s<=50)
+		{
+			Console.WriteLine("average");
+		}
+		else if(s>50 & s<=150)
+		{
+			Console.WriteLine("fast");
+		}
+		else if(s>150 & s<=1000)
+		{
+			Console.WriteLine("ultra fast");
+		}
+		else if(s>1000)
+		{
+			Console.WriteLine("extremely fast");
+		}
+		
+		
+	}
+}
