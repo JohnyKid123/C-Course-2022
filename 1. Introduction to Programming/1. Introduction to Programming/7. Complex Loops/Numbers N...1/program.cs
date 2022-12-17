f359cac6a6a0ef48ee23591fb15b0fb315c91384
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 20:24:36 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/7. Complex Loops/Numbers N...1/program.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		for(int i=n; i>=1; i-=1)
+			Console.WriteLine(i);
+		
+	}
+}
