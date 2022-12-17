d02d2d81628a5da15dbb6460e80c2f5a2dad92bb
JohnyKid123 <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 10:27:17 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/2. Simple Calculations/Trapeziod Area/program.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var a = double.Parse(Console.ReadLine());
+		var b = double.Parse(Console.ReadLine());
+		var h = double.Parse(Console.ReadLine());
+		Console.WriteLine((a+b)/2*h);
+	}
+						  
+}
