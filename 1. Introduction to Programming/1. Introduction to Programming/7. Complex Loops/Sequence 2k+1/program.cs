87f32049cc1d24014bc3869deeddcd98347aa022
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 20:26:23 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/7. Complex Loops/Sequence 2k+1/program.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		int num = 1;
+		while (num <= n)
+			{
+				Console.WriteLine(num) ;
+				num = 2 * num + 1;
+			}		
+	}
+}
