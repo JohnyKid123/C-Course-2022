937ca0ada05c39738d3a393829014bcd5ea7fe41
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 18:27:24 +0200

    Update program.cs

--- 1. Introduction to Programming/1. Introduction to Programming/2. Simple Calculations/1000 Days After Birth/program.cs
+++ 1. Introduction to Programming/1. Introduction to Programming/2. Simple Calculations/1000 Days After Birth/program.cs
@@ -7,6 +7,6 @@ class ThousandDayAfter
         string ftm = "dd-MM-yyyy";
         string value = Console.ReadLine();
         DateTime userBirthday = DateTime.ParseExact(value, ftm, null);
-        Console.WriteLine(userBirthday.AddDays(999).ToString("dd-MM-yyyy"));
+        Console.WriteLine(userBirthday.AddDays(1000).ToString("dd-MM-yyyy"));
     }
 }
