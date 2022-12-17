8006887065dd5c2fae5805ce617a521b3fcf4502
JohnyKid123 <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 10:18:29 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/1. First Steps in Programming/Rectangle Area/program.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var a = double.Parse(Console.ReadLine());
+		var b = double.Parse(Console.ReadLine());
+		Console.WriteLine(a*b);
+	}
+						  
+}
