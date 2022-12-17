afbcde5cbd9e40d1c9e405f7d6056896e50c2090
JohnyKid123 <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 10:22:54 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/2. Simple Calculations/Square Area/program.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var a = double.Parse(Console.ReadLine());
+		var area = a*a;
+		Console.WriteLine(area);
+	}
+						  
+}
