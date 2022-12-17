c8baf19453a23ee9a38b033a86b1a90c74bee199
JohnyKid123 <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 10:26:45 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/2. Simple Calculations/Concatenate Data/program.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var first_name = Console.ReadLine();
+		var last_name = Console.ReadLine();
+		var ears = Console.ReadLine();
+		var city = Console.ReadLine();
+		Console.WriteLine("You are" + " " + first_name + " " + last_name + ", a" + " " + ears + "-years old person from" + " " +city + ".");
+		
+	}
+						  
+}
