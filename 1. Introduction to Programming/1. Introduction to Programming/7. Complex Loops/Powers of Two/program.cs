db3a9198ce9bb1f5a83381fb7f68ccffae97fea5
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-17 20:25:05 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/1. Introduction to Programming/7. Complex Loops/Powers of Two/program.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var num = 1;
+		for(int i=0; i<=n; i++)
+		{
+			Console.WriteLine(num);
+			num = num*2;
+		}
+	}
+}
