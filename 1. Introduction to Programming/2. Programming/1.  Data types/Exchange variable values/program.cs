ba2c4e330ca82c900ae20bd442e73ced74e2ed52
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-29 22:43:50 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/1.  Data types/Exchange variable values/program.cs
@@ -0,0 +1,8 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Before:");
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine($"After:");
+Console.WriteLine($"a = {b}");
+Console.WriteLine($"b = {a}");
