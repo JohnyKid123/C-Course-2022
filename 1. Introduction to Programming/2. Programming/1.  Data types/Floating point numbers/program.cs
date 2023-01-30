ab0c7441fbe299680c4cb159886c87f42defcebc
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-30 18:03:24 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/1.  Data types/Floating point numbers/program.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program {
+    public static void Main(string[] args) 
+    {
+        decimal num = decimal.Parse(Console.ReadLine());
+decimal num1 = decimal.Parse(Console.ReadLine());
+decimal num2 = decimal.Parse(Console.ReadLine());
+Console.WriteLine(num);
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+
+    }
+}
