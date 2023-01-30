6444bafadc2215e7cf955e46fed6feb81f1893f7
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-30 17:49:33 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/1.  Data types/Whole numbers/program.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Program {
+    public static void Main(string[] args) 
+    {
+        short num = -100;
+byte num1 = 128;
+short num2 = -3540;
+int num3 = 64876;
+long num4 = 2147483648;
+var num5 = -1141583228;
+var num6 = -1223372036854775808;
+Console.WriteLine(num);
+Console.WriteLine(num1);    
+Console.WriteLine(num2);
+Console.WriteLine(num3);
+Console.WriteLine(num4);
+Console.WriteLine(num5);
+Console.WriteLine(num6);
+
+    }
+    
+}
