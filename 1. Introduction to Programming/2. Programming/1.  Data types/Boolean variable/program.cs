093d1a31e2332381c6e0fd65396f46019f7d614e
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-31 23:33:27 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/1.  Data types/Boolean variable/program.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program {
+    public static void Main(string[] args) 
+    {
+        string input = Console.ReadLine();
+bool variable = Convert.ToBoolean(input);
+if (variable == true) {
+Console.WriteLine("Yes");
+} else {
+Console.WriteLine("No");
+}
+
+    }
+}
