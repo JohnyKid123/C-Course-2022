1f8d66b1d12f0b00ef77b576ed83b9c214446bad
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-02-16 20:26:48 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/1.  Data types/Greeding/program.cs
@@ -0,0 +1,5 @@
+
+string first = Console.ReadLine();
+string last = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine($"Hello, {first} {last}. You are {age} years old.");
