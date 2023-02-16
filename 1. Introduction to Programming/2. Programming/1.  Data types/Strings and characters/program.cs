f980da04e4a70bb3a696fd6d5e8b8fe2548fcb10
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-02-16 20:29:17 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/1.  Data types/Strings and characters/program.cs
@@ -0,0 +1,11 @@
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+string d = Console.ReadLine();
+string e = Console.ReadLine();
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+
