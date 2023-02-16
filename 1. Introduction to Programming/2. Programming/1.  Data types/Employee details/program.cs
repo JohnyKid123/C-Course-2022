96e59f670ab638c9b385f9e54aa626aea531c4ca
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-02-16 20:40:06 +0200

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/1.  Data types/Employee details/program.cs
@@ -0,0 +1,13 @@
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+int c = int.Parse(Console.ReadLine());
+char d = char.Parse(Console.ReadLine());
+double e = double.Parse(Console.ReadLine());
+double f = double.Parse(Console.ReadLine());
+Console.WriteLine($"First Name: {a}");
+Console.WriteLine($"Last Name: {b}");
+Console.WriteLine($"Age: {c}");
+Console.WriteLine($"Gender: {d}");
+Console.WriteLine($"Personal ID: {e}");
+Console.WriteLine($"Unique Employee number: {f}");
+
