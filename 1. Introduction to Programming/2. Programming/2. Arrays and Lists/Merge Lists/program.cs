790d62644a55f4c3deda521ba9d9a4c39560e844
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-04-12 22:46:12 +0300

    Create program.cs

---
+++ 1. Introduction to Programming/2. Programming/2. Arrays and Lists/Merge Lists/program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listDefinitions = Console.ReadLine().Split('|');
+            List<int> numbers = new List<int>();
+
+            for (int i = listDefinitions.Length - 1; i >= 0; i--)
+            {
+                int[] currentNumbers = listDefinitions[i]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                // numbers.AddRange(currentNumbers);
+                foreach (int num in currentNumbers)
+                    numbers.Add(num);
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
