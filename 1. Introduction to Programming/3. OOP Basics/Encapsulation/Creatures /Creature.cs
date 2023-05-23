a9508ba524cba388659173d9cff37ea59f2a1435
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-05-23 22:45:46 +0300

    Create Creature.cs

---
+++ 1. Introduction to Programming/3. OOP Basics/Encapsulation/Creatures /Creature.cs
@@ -0,0 +1,9 @@
+namespace Creatures
+{
+    public class Creature
+    {
+        public string Name { get; set; }
+        public int Years { get; set; }
+        public string Arial { get; set; }
+    }
+}
